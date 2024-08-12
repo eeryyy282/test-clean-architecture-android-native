a9f7d3408d5827a613e12b493e89a086d95937d5
Airi <airiagustus82@gmail.com>
2024-08-12 07:04:36 +0700

    Add binding feature, add viewmodel and message in MainActivity

--- app/src/main/java/com/learnjetpackcompose/testcleanarchitectureandroidnative/presentation/MainActivity.kt
+++ app/src/main/java/com/learnjetpackcompose/testcleanarchitectureandroidnative/presentation/MainActivity.kt
@@ -5,13 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.learnjetpackcompose.testcleanarchitectureandroidnative.R
+import com.learnjetpackcompose.testcleanarchitectureandroidnative.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val factory = MainViewModelFactory.getInstace()
+        val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+
+        viewModel.setName("Juzairi")
+        viewModel.message.observe(this) {
+            binding.tvWelcome.text = it.welcomeMessage
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
